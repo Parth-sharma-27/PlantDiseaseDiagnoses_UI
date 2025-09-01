9d6ecf8d901a8c0573b7f623bdb884dbdad85a98
vansh242 <vanshpratap886@gmail.com>
2025-09-01 13:36:58 +0530

    Local

---
+++ KhetSaathi.Models/ViewModels/VoiceQuestionVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhetSaathi.Models.ViewModels
+{
+    public class VoiceQuestionVM
+    {
+        public string Question { get; set; }
+        public string Lang { get; set; }
+    }
+}
